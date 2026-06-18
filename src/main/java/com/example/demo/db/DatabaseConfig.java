2ee7ff83b71573b44a8f514b10df812cfa973273
valisoa01 <valisoatolotriniaina@gmail.com>
2026-06-18 10:37:34 +0300

    add db-connexion

---
+++ src/main/java/com/example/demo/db/DatabaseConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+
+    private static final String URL = System.getenv("NEON_DB_URL");
+    private static final String USERNAME = System.getenv("NEON_DB_USERNAME");
+    private static final String PASSWORD = System.getenv("NEON_DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        if (URL == null || USERNAME == null || PASSWORD == null) {
+            throw new IllegalStateException(
+                    "Database env vars not defined: NEON_DB_URL, NEON_DB_USERNAME, NEON_DB_PASSWORD");
+        }
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
--- src/main/java/com/example/demo/endpoint/rest/controller/health/PingController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/PingController.java
@@ -1,11 +1,14 @@
 package com.example.demo.endpoint.rest.controller.health;
 
 import com.example.demo.PojaGenerated;
+import com.example.demo.db.DatabaseConfig;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
 
 @PojaGenerated
 @RestController
@@ -19,5 +22,14 @@ public class PingController {
   @GetMapping("/ping")
   public String ping() {
     return "pong";
+  }
+
+  @GetMapping("/ping-db")
+  public ResponseEntity<String> pingDb() {
+    try (Connection conn = DatabaseConfig.getConnection()) {
+      return ResponseEntity.ok("Connexion Neon OK !");
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body("Erreur : " + e.getMessage());
+    }
   }
 }
