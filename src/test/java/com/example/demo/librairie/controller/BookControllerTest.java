8f5775f47c0b421f619062c8a9ee69f7db844ed0
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-06-21 21:23:04 +0300

    chore : configuration

---
+++ src/test/java/com/example/demo/librairie/controller/BookControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.librairie.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookControllerTest {
+
+  @Test
+  void getAll() {}
+
+  @Test
+  void getById() {}
+
+  @Test
+  void getByTitle() {}
+
+  @Test
+  void getByGenre() {}
+
+  @Test
+  void getByDate() {}
+
+  @Test
+  void create() {}
+
+  @Test
+  void update() {}
+
+  @Test
+  void delete() {}
+}
