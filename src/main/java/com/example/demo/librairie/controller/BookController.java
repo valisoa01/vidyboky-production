aebc7da1a00a4196e14c4e8abfcb5cc301818baa
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-06-21 19:24:49 +0300

    chore : change error response in book

--- src/main/java/com/example/demo/librairie/controller/BookController.java
+++ src/main/java/com/example/demo/librairie/controller/BookController.java
@@ -33,7 +33,9 @@ public class BookController {
   }
 
   @GetMapping("/{id}")
-  public BookResponse getById(@PathVariable UUID id) {return  toResponse(bookService.getById(id));}
+  public BookResponse getById(@PathVariable UUID id) {
+    return toResponse(bookService.getById(id));
+  }
 
   @GetMapping("/searchByTitle")
   public ResponseEntity<List<BookResponse>> getByTitle(@RequestParam String title) {
