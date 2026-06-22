ebd702f203f20062b2d0c83ced5935da87442edf
valisoa01 <valisoatolotriniaina@gmail.com>
2026-06-22 11:12:20 +0300

    add FormatControllerTest

---
+++ src/test/java/com/example/demo/librairie/controller/FormatControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.librairie.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormatControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
