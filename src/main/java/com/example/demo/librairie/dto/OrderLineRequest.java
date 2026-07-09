b3556bcfd9024776710318c49cf62346293b8f80
Lahatranomena <hei.lahatra.3@gmail.com>
2026-07-09 16:15:24 +0300

    feat(dto): add OrderLineRequest

---
+++ src/main/java/com/example/demo/librairie/dto/OrderLineRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo.librairie.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderLineRequest {
+
+    @NotNull
+    private UUID orderId;
+
+    @NotNull
+    private UUID bookFormatId;
+
+    @NotNull
+    @Positive
+    private Integer quantity;
+}
