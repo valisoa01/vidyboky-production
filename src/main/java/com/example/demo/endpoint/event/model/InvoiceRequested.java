32b18f7803aedcc8bfa06a39dc6a1aef76f10b6c
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-07-09 21:58:38 +0300

    feat(invoice): trigger async invoice generation and email after payment

--- src/main/java/com/example/demo/endpoint/event/model/InvoiceRequested.java
+++ src/main/java/com/example/demo/endpoint/event/model/InvoiceRequested.java
@@ -1,4 +1,24 @@
 package com.example.demo.endpoint.event.model;
 
-public class InvoiceRequested {
+import java.time.Duration;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class InvoiceRequested extends PojaEvent {
+  private String orderId;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(60);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
 }
--- src/main/java/com/example/demo/librairie/service/PaymentService.java
+++ src/main/java/com/example/demo/librairie/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.example.demo.librairie.service;
 
+import com.example.demo.endpoint.event.EventProducer;
+import com.example.demo.endpoint.event.model.InvoiceRequested;
 import com.example.demo.librairie.dto.PaymentRequest;
 import com.example.demo.librairie.dto.PaymentResponse;
 import com.example.demo.librairie.entity.Order;
@@ -17,7 +19,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
-
+  private final EventProducer eventProducer;
   private final PaymentRepository paymentRepository;
   private final OrderRepository orderRepository;
 
@@ -60,7 +62,12 @@ public class PaymentService {
             .order(order)
             .build();
 
-    return toResponse(paymentRepository.save(payment));
+    Payment savedPayment = paymentRepository.save(payment);
+
+    eventProducer.accept(
+        List.of(InvoiceRequested.builder().orderId(order.getId().toString()).build()));
+
+    return toResponse(savedPayment);
   }
 
   public void delete(UUID id) {
