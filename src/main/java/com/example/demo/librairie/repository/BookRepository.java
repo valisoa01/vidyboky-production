c4937df13fe1fbe405f15d09108944b52ae4d09b
miorajulia01 <miorahanitra01.com>
2026-07-17 00:01:15 +0300

    feat: update BookRepository , add BookExternalService

--- src/main/java/com/example/demo/librairie/repository/BookRepository.java
+++ src/main/java/com/example/demo/librairie/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.librairie.repository;
 import com.example.demo.librairie.entity.Book;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -17,4 +18,6 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
   List<Book> findByAuthors_Id(UUID authorId);
 
   List<Book> findByGenres_Id(UUID genreId);
+
+  Optional<Book> findByIsbn(String isbn);
 }
